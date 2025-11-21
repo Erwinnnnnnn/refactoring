93ad930479368c0297bc062894a0d7038562283e
erwin <erzheng.zhang@mail.utoronto.ca>
2025-11-21 12:08:23 -0500

    Task 2.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -81,4 +81,35 @@ public class StatementPrinter {
 
         return result.toString();
     }
+
+    public int getAmount() {
+        int totalAmount = 0;
+        for (final Performance p : invoice.getPerformances()) {
+            final Play play = plays.get(p.getPlayID());
+            int thisAmount = 0;
+            switch (play.getType()) {
+                case "tragedy":
+                    thisAmount = Constants.TRAGEDY_BASE_AMOUNT;
+                    if (p.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                        thisAmount += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
+                                * (p.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                    }
+                    break;
+                case "comedy":
+                    thisAmount = Constants.COMEDY_BASE_AMOUNT;
+                    if (p.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                        thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
+                                + Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                                * (p.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD);
+                    }
+                    thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * p.getAudience();
+                    break;
+                default:
+                    throw new RuntimeException("unknown type: " + play.getType());
+            }
+            totalAmount += thisAmount;
+        }
+        return totalAmount;
+    }
+
 }
